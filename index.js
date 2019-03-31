2ed78dbe2ee9aa6980a7f87b9c646a60fa293171
udom <udomdev@gmail.com>
2019-03-31 13:07:52 +0700

    change model url

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ let model;
 // we'll use as input to our classifier model.
 async function loadTruncatedMobileNet() {
   const mobilenet = await tf.loadLayersModel(
-      'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+      'https://raw.githubusercontent.com/udomdev/tfjs-pacman/master/mobilenet_1/model.json');
 
   // Return a model that outputs an internal activation.
   const layer = mobilenet.getLayer('conv_pw_13_relu');
